7c064b4e81ee00afb3fe284d9d9667e9aa565c1e
Toyosi <Badmustoyosi56@gmail.com>
2026-08-28 17:58:10 +0100

    Fix production database configuration

--- config/settings.py
+++ config/settings.py
@@ -51,11 +51,13 @@ TEMPLATES = [
 
 WSGI_APPLICATION = 'config.wsgi.application'
 
+DATABASE_URL = config('DATABASE_URL', default='')
+
 DATABASES = {
-    'default': dj_database_url.config(
-        default=f'sqlite:///{BASE_DIR / "db.sqlite3"}',
+    'default': dj_database_url.parse(
+        DATABASE_URL,
         conn_max_age=600,
-        ssl_require=False,
+        ssl_require=True,
     )
 }
 
